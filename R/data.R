2babf801d6f8c3e969d808872257c10d68128dbd
mgdesaix <mgdesaix@gmail.com>
2021-11-22 16:44:06 -0700

    update data and vignette

--- R/data.R
+++ R/data.R
@@ -19,3 +19,20 @@
 #' }
 #' @source \url{https://ebird.org/science/status-and-trends/comyel}
 "comyel_breed_smooth"
+
+#' Q-values and metadata for the Common Yellowthroat samples
+#'
+#' The tibble provides metadata for all of the individuals used to create the genoscape map.
+#' Each individual has a unique identifier, location data, and Q values provided from Structure.
+#'
+#' @format A tibble with 234 rows, 14 columns:
+#' \describe{
+#'   \item{Sample}{Individual identifier}
+#'   \item{Assignment}{Assigned genetic cluster based on Q-values with a threshold of 0.7}
+#'   \item{CA}{Q-values for California cluster}
+#'   \item{Midwest}{Q-values for Midwest cluster}
+#'   \item{NewEngland}{Q-values for New England cluster}
+#'   \item{West}{Q-values for West cluster}
+#'   \item{Southwest}{Q-values for Southwest cluster}
+#' }
+"comyel_assignment"

8381299e86b69a7baf2325a5b2f221fb33a9538a
mgdesaix <mgdesaix@gmail.com>
2024-05-03 18:04:43 -0600

    streamlined raster abundance calculation

--- R/get_raster_abunds.R
+++ R/get_raster_abunds.R
@@ -3,54 +3,34 @@
 #' @param populations SpatRaster object of layers with values delineating membership probabilities to a population
 #' @param abunds SpatRaster object of ebirdst abundance
 #' @param pop_names Character vector of raster layer names manually provided. By default the function obtains this from `names(populations)`.
-#' @return A two-column matrix with population names in the first column and relative abundance values in the second column
+#' @return A two-column data frame with population names in the first column and relative abundance values in the second column
 #' @export
 #'
 #'
 get_raster_abunds <- function(populations, abunds, pop_names = names(populations)){
   stopifnot("`populations` layers and `pop_names` file of different lengths!" = length(names(populations)) == length(pop_names))
   spp_pops <- pop_names
-  # Load rasters of q values
-  for(i in 1:terra::nlyr(populations)){
-    populations[[i]][populations[[i]] < 0] <- 0
-  }
-  # Vectorize rasters of q values
-  genoscape_vect <- terra::as.polygons(populations, dissolve=F)
 
-  # Rescale q values to probabilities using the function P = exp(-(b/qval))/sum(exp(-(b/qval)))
   b <- 1
-  # The value of b is set above
-  terra::values(genoscape_vect) <- as.data.frame(t(apply(terra::values(genoscape_vect), 1, function(x) exp(-(b/x))/sum(exp(-(b/x))))))
-
-  abunds <- terra::project(abunds, '+proj=longlat +datum=WGS84', method = "near")
-  abunds[abunds > 1000] <- 0
-
-  terra::crs(populations) <- terra::crs(genoscape_vect) <- terra::crs(abunds) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-
   # Change extent
-  abunds_br <- terra::crop(abunds, terra::ext(populations))
+  # abunds_br <- terra::crop(abunds, terra::ext(populations))
+  populations_01 <- populations %>%
+    terra::clamp(lower = 0) %>% # classify less than 0 to 0
+    terra::app(., fun=function(x) exp(-(b/x))/sum(exp(-(b/x)))) # rescale probabilities 0-1
 
-  # Get pop size per pixel
-  abunds_br_genoscape <- terra::extract(abunds_br, genoscape_vect, weights=T, fun=sum, na.rm=TRUE)
-  genoscape_vect$popsize <- abunds_br_genoscape[,2]
-  # Rasterize the genoscape
-  genoscape2 <- terra::rasterize(genoscape_vect, populations, colnames(terra::values(genoscape_vect))[1])
-  for(i in 2:ncol(genoscape_vect)){
-    genoscape2 <- c(genoscape2, terra::rasterize(genoscape_vect, populations, colnames(terra::values(genoscape_vect))[i]))
-  }
+  # standardize abundance pixels to genoscape pixels (by summing)
+  abundance_br <- terra::resample(abunds, populations_01, method="sum")
 
-  # Abundance surface per genetically distinct population
-  for(i in 1:(terra::nlyr(genoscape2)-1)){
-    genoscape2 <- c(genoscape2, genoscape2[[i]] * genoscape2[[terra::nlyr(populations)+1]])
-  }
-  names(genoscape2) <- c(spp_pops, "relative_abundance", paste0(spp_pops, "_abund"))
+  # calculate weighted abundance
+  abunds_weighted_raster <- abunds_br * populations_01
 
-  # Breeding abundance per genetically distinct population
-  spp_pops_abunds <- cbind(spp_pops,
-                           apply(terra::values(genoscape2)[,(ceiling(terra::nlyr(genoscape2)/2)+1):terra::nlyr(genoscape2)], 2, function(x) sum(x, na.rm=T))
-  )
+  # summarize abundance
+  final_abunds <- abunds_weighted_raster %>%
+    terra::values() %>%
+    apply(., 2, function(x) sum(x, na.rm = T)) %>%
+    as.numeric()
 
-  colnames(spp_pops_abunds) <- c("Population", "Relative_abundance")
-  rownames(spp_pops_abunds) <- NULL
-  return(spp_pops_abunds)
+  final_abunds_df <- data.frame("Population" = spp_pops,
+                                "Relative_abundance" = final_abunds)
+  return(final_abunds_df)
 }

a9e4d8f6cb746970b0e584f2fa827b4235a07513
mgdesaix <mgdesaix@gmail.com>
2024-05-21 12:56:47 -0600

    update test

--- tests/testthat/test-get_jags_model.R
+++ tests/testthat/test-get_jags_model.R
@@ -5,7 +5,7 @@ test_that("get_jags_model works", {
 
 
 test_that("get_vector_abunds works", {
-  expect_equal(as.numeric(get_vector_abunds(populations = terra::vect(rbind(c(0,0), c(0,5), c(5,5), c(5,0)), "polygons", crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"),
-                                 abunds = terra::rast(matrix(1,nrow=10,ncol=10), crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"),
+  expect_equal(round(get_vector_abunds(populations = terra::vect(rbind(c(0,0), c(0,5), c(5,5), c(5,0)), "polygons", crs = "EPSG:4326"),
+                                 abunds = terra::rast(matrix(1,nrow=10,ncol=10), crs = "EPSG:4326"),
                                  pop_names = "cat")[1,2]), 25)
 })
